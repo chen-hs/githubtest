8f7809cba37d4754a7c14d3e2ad83ed462276bae
chen-hs <468453790@qq.com>
2020-05-25 23:37:26 +0800

    new change

--- app/src/main/java/com/example/administrator/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/LoginActivity.java
@@ -2,8 +2,13 @@ package com.example.administrator.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView txt;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        txt = (TextView)this.findViewById(R.id.textView);
+        btn = (Button) this.findViewById(R.id.button);
     }
 }
